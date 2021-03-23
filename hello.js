78ba062dd70da7be3b783ca897d0a7574d44849a
s9one1 <s9one1@gmail.com>
2021-03-23 12:48:06 +0900

    변경

--- hello.js
+++ hello.js
@@ -1,5 +1,14 @@
-/*console.log('hello world...!');
-console.log('hello world...!');
-console.log('hello world...!');
-console.log('hello world...!');
-*/
+let date = new Date();
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDay());
+console.log(date.getTime());
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+if (date.getHours()<12){
+    console.log("오전!");
+}
+console.log("오후!");
